5477650809e0dc56542ff4e7b440cebe6ff457c7
Craig Belton II <csbeltonii@gmail.com>
2025-06-19 18:35:15 -0400

    dropped to .NET 8

--- samples/Orbital.Sample.WebApi/Program.cs
+++ samples/Orbital.Sample.WebApi/Program.cs
@@ -29,16 +29,8 @@ builder.Services
        .AddCosmosRepositories();
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 app.MapSampleItemEndpoints();
